7e153474db3ff94779cdbc933fe8e1027b36318b
Petar Petrevski <petarpetrevski99@gmail.com>
2022-05-19 15:24:29 +0200

    Implemented Sign-In with Email functionality

--- app/src/main/java/com/petarpetrevski/socialmediaapp/SignInActivity.kt
+++ app/src/main/java/com/petarpetrevski/socialmediaapp/SignInActivity.kt
@@ -1,10 +1,14 @@
 package com.petarpetrevski.socialmediaapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,46 @@ class SignInActivity : AppCompatActivity() {
 
         sign_up_link_button.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        sign_in_button.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email = email_sign_in.text.toString()
+        val password = password_sign_in.text.toString()
+
+        when {
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "Email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "Password is required.", Toast.LENGTH_LONG).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this@SignInActivity)
+                progressDialog.setTitle("Signing In")
+                progressDialog.setMessage("Please wait...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        progressDialog.dismiss()
+
+                        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        val errorMessage = task.exception!!.toString()
+                        Toast.makeText(this, "Error:  $errorMessage", Toast.LENGTH_LONG).show()
+                        FirebaseAuth.getInstance().signOut()
+                        progressDialog.dismiss()
+                    }
+                }
+            }
         }
     }
 
